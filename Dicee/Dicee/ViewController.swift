a914cb87e076e8379a231acc2ea00e71dcb6e817
NIKMC <nikmc10@mail.ru>
2018-01-24 23:17:13 +0300

    Constraints have been fixed

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateDiceImages()
+        
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
